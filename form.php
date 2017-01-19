c840c647b385c8d87eca45cd29632ec028ae875b
Shaad Khan <shaad39@gmail.com>
2017-01-20 03:30:41 +0530

    q

--- form.php
+++ form.php
@@ -35,7 +35,7 @@
 	<!--<div class="col-xs-3">Ticket Id <span class="glyphicon glyphicon-info-sign"></span> </div><div class="col-xs-3"><input type="text"/></div>
 	<div class="col-xs-3">Created By <span class="glyphicon glyphicon-info-sign"></span> </div><div class="col-xs-3"><input type="text"/></div>
 	-->
-<div class="col-xs-4">
+<div class="col-xs-6">
 
 <div class="form-group">
     <label for="exampleInputEmail1">Ticket Id <span class="glyphicon glyphicon-info-sign
@@ -44,20 +44,16 @@
   </div>
 
 </div>
-<div class="col-xs-4">
+<div class="col-xs-6">
 
 <div class="form-group">
-    <label for="exampleInputEmail1">Ticket Id</label>
+    <label for="exampleInputEmail1">Ticket Id <span class="glyphicon glyphicon-info-sign
+"></span></label>
     <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Ticket Id">
   </div>
 
 </div>
-<div class="col-xs-4">
 
-<div class="form-group">
-    <label for="exampleInputEmail1">Ticket Id</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Ticket Id">
-  </div>
 
 </div>
 
