5987d805e90adc94e396658cda7dc2d0a5e39271
Shaad Khan <shaad39@gmail.com>
2017-01-19 18:34:58 +0530

    changes

--- home.php
+++ home.php
@@ -27,13 +27,13 @@
 <div class="content-panel" id="reload"  >
 
 
-                         <div class="row">
+                         <div class="row" ng-init="load='true'">
 
                             <div class="col-xs-12" style="padding-left:40px;padding-right:40px;">
                               <div class="panel panel-primary">
                                 <div class="panel-heading" style="background-color:#001a33"><div class="row"><div class="col-xs-8">
 
-                                  Task Report </div><div class="col-xs-4" align="right" ng-hide="False"><img src="assets/ajax-loader.gif"/>
+                                  Task Report </div><div class="col-xs-4" align="right" ng-hide="load"><img src="assets/ajax-loader.gif"/>
                                   </div></div></div>
       <div class="panel-body">
         <table class="table"><tr>
