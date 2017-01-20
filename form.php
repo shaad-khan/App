1376209a7d6fbe61990dd3a896e473eed196a991
Shaad Khan <shaad39@gmail.com>
2017-01-20 18:02:00 +0530

    c

--- form.php
+++ form.php
@@ -268,7 +268,7 @@
     <div class="panel-heading" role="tab" id="headingTwo">
       <h4 class="panel-title">
         <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        <span class="glyphicon glyphicon-pushpin"> </span> Updater Name: Ashish Kumar 2 [DateTime:20/01/2017 8:30 pm]
+        <span class="glyphicon glyphicon-pushpin"> </span> Updater Name: Ashish Kumar  [DateTime:20/01/2017 8:30 pm]
         </a>
       </h4>
     </div>
