6f18deb58ae80776441f3e24fdee37b6f6220a30
Shaad Khan <shaad39@gmail.com>
2017-01-20 17:53:59 +0530

    z

--- form.php
+++ form.php
@@ -278,7 +278,7 @@
       </div>
     </div>
   </div>
-  <div class="panel panel-default">
+  <div class="panel panel-info">
     <div class="panel-heading" role="tab" id="headingThree">
       <h4 class="panel-title">
         <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
