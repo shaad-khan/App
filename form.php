c56c0c574e4bc99772b4064bb237f96950d4f711
Shaad Khan <shaad39@gmail.com>
2017-01-20 16:38:32 +0530

    z

--- form.php
+++ form.php
@@ -250,7 +250,7 @@
  <div class="col-xs-12">
   <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
   Button with data-target
-</button>
+</button><div class="clear"></div>
 <div class="collapse" id="collapseExample">
   <div class="well">
     ...
