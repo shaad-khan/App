ff72c3c2172cb3aefbe2a96d654741563b633cfb
Shaad Khan <shaad39@gmail.com>
2017-01-22 00:05:42 +0530

    z

--- angular/controller.js
+++ angular/controller.js
@@ -6,7 +6,7 @@ app.controller("Ticket",function($scope,$routeParams,$http,$interval){
 		var id=ID;
 		var url="form.php?ID="+id;
 	//	alert(url);
-	  newwindow=window.open(url,'name','height=600,width=1100');
+	  newwindow=window.open(url,'name','height=600,width=1200');
 	  if (window.focus) {newwindow.focus()}
 	  return false;
 	};
