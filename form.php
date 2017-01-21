78d45433084cfaed9ebba4083292e9c749172af8
Shaad Khan <shaad39@gmail.com>
2017-01-21 23:38:25 +0530

    z

--- form.php
+++ form.php
@@ -38,7 +38,7 @@
 <br>
   <div class="panel panel-primary">
     <div class="panel-heading">Edit Form For Ticket ID: <?php Echo $ID;?></div>
-    <div class="panel-body"><form> 
+    <div class="panel-body"><form class="form-inline"> 
 
 <table>
 <tr><td>
