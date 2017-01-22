c9abc48d6b3b24bf8f9939e499ed0693c3408292
Shaad Khan <shaad39@gmail.com>
2017-01-23 02:54:23 +0530

    z

--- form.php
+++ form.php
@@ -71,8 +71,8 @@ td
     <div class="form-group" ng-controller="client">
     <label for="exampleInputEmail1">Client  <span class="glyphicon glyphicon-info-sign
 "></span></label></td><td>
-    <select class="form-control" ng-repeat="list in lists">
-  <option>{{list.Client_name}}</option>
+    <select class="form-control" >
+  <option ng-repeat="list in lists" >{{list.Client_name}}</option>
 </select>
   </div>
   </td>
