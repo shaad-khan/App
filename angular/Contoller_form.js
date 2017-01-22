7bac340fabf25dee1996987bb5155dd5b6391bce
Shaad Khan <shaad39@gmail.com>
2017-01-23 01:18:38 +0530

    z

--- angular/Contoller_form.js
+++ angular/Contoller_form.js
@@ -3,15 +3,18 @@ var app2=angular.module("continuity_form",[]);
 app2.controller("Form_data",function($scope){
     
 
-    alert($scope.ID);
-    $scope.ID="xxx";
-/* $interval(callApi, 5000);
+   
+$interval(callApi, 5000);
 
   function callApi(){
-    $http.get("https://apps.continuserve.com/webservice/ticket_data.php?ID="+$scope.ID).then(function(response){
+    /*$http.get("https://apps.continuserve.com/webservice/ticket_data.php?ID="+$scope.ID).then(function(response){
      	$scope.load2='false';
       $scope.items=response.data;
-    });
-  }*/
+
+    });*/
+      console.log($scope.ID);
+   // $scope.ID="xxx";
+
+  }
 
 });
