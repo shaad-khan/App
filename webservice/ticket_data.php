8a0057b63d6bcbc3701e7dc41501f4c4720f5d60
Shaad Khan <shaad39@gmail.com>
2017-01-23 00:39:38 +0530

    z

--- webservice/ticket_data.php
+++ webservice/ticket_data.php
@@ -14,8 +14,8 @@ $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
 
 
     $sql="select * from Master_Ticket_Tab where Target_ID='".$id."'";
-    //echo $sql;
-$result=$conn->query($sql);
+    echo $sql;
+/*$result=$conn->query($sql);
 //echo $msg;
   while($row4=$result->fetch())
 {
@@ -25,5 +25,5 @@ $result=$conn->query($sql);
 
 
 
-print(json_encode($rows, JSON_NUMERIC_CHECK));
+print(json_encode($rows, JSON_NUMERIC_CHECK));*/
 ?>
