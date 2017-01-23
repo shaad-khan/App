62c78bfb9f61ca92a2dc31f9282d4ef09d9e76b6
Shaad Khan <shaad39@gmail.com>
2017-01-23 16:06:03 +0530

    z

--- angular/Contoller_form.js
+++ angular/Contoller_form.js
@@ -8,10 +8,11 @@ app.controller("Form_data",function($scope,$interval,$http,servicecall){
 
     servicecall.serv("shiftschedule").then(function(response){
 $scope.schedules=response.data;
+});
 service.call.serv("Task_Category").then(function(response){
 $scope.tasks=response.data;
 });
-    });
+    
     
 $interval(callApi, 1000);
 
