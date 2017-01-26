1983485f96982046f5deb6f0d04ba7fc08c96021
Shaad Khan <shaad39@gmail.com>
2017-01-27 02:21:29 +0530

    z

--- form.php
+++ form.php
@@ -101,7 +101,7 @@ td
 <div class="form-group">
     <label for="exampleInputEmail1">Ticket Id <span class="glyphicon glyphicon-info-sign
 "></span></label></td><td>
-  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="<?php Echo $ID;?>" name="TID" disabled>
+  <input type="text" class="form-control" name="TID" id="exampleInputEmail1" placeholder="<?php Echo $ID;?>"  disabled>
   </div>
 
 </div></td>
