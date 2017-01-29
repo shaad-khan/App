3cc9d2318740e605a58f84a611fe2df05d8a78a3
Shaad Khan <shaad39@gmail.com>
2017-01-29 22:26:43 +0530

    z

--- webservice/Tab_content.php
+++ webservice/Tab_content.php
@@ -70,10 +70,14 @@ $sql3="select count(*) as ccount from dbo.Master_Ticket_Tab where Creator like '
 	$sql="update Status_count set Classify=".$row3['ccount']." where Email like '%".$r."%'";
 	//echo $resolver[0].",Classify: $row3['ccount']";
 	//echo $sql;
-	$conn->query($sql);
+	
 	//=$resolver[0].",Classify: $classify,wip:$wip,aui:$auc";
 
 	}
+  else{
+    $sql="update Status_count set Classify=0 where Email like '%".$r."%'";
+  }
+  $conn->query($sql);
 	//$rows[]=$row3;
 
 
