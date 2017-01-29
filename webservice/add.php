4475c249f01f89ef2bf2812364a2c5744843e56e
Shaad Khan <shaad39@gmail.com>
2017-01-29 19:52:39 +0530

    x

--- webservice/add.php
+++ webservice/add.php
@@ -124,7 +124,7 @@ $Master_sql="Update Master_Ticket_Tab set Assigned_To='unassigned',Status='$fsta
 
 echo $Master_sql;
 
-	$conn->query($Master_sql);
+    echo ($conn->query($Master_sql));
 
 //$sql="insert into Update_Tab values('$TID','$fstatus','$utime','$uname','$schedule','$client','$project','$ttime','$reviewer','$resolver','','$tcategory')";
 
