9c3dc71c7dbdeddcfced9dec12daee97695df471
Shaad Khan <shaad39@gmail.com>
2017-01-29 16:52:08 +0530

    n

--- webservice/add.php
+++ webservice/add.php
@@ -56,8 +56,8 @@ $AUI= $_GET["AUI"];
 if(($creator!=$user_session)and($status=='Classify'))
 {
    //echo "i am step 1";
-     echo "<script> alert('ticket is assigned to user');
-     setTimeout(function(){window.close()}, 1000);
+     echo "<script> alert('ticket is assigned to $user');
+     setTimeout(function(){window.close()}, 2000);
      </script>";
     
 }
