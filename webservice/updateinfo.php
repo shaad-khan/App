4eff5765170fbe036206ca1f9a92033de3e48bac
Shaad Khan <shaad39@gmail.com>
2017-02-01 04:53:44 +0530

    b

--- webservice/updateinfo.php
+++ webservice/updateinfo.php
@@ -13,7 +13,7 @@ $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
 
 
 
-    $sql="select * from Update_Tab where TicketId='".$id."'";
+    $sql="select * from Update_Tab where TicketId='".$id."' order by desc";
     //echo $sql;
 $result=$conn->query($sql);
 //echo $msg;
