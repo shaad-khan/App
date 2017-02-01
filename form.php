2ac45ab73fa4f127363bc9f09314777bf1bde820
Shaad Khan <shaad39@gmail.com>
2017-02-01 15:09:47 +0530

    c

--- form.php
+++ form.php
@@ -489,6 +489,9 @@ td
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+     
      <div class="panel panel-default">
   <div class="panel-body">
     
