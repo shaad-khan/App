6c6e44df29d025e26643116e22009d9a2d6134ae
Shaad Khan <shaad39@gmail.com>
2017-02-01 17:52:11 +0530

    c

--- webservice/Tab_content.php
+++ webservice/Tab_content.php
@@ -135,6 +135,34 @@ $sql3="select count(*) as ccount from dbo.Master_Ticket_Tab where Assign_to like
 
 
 }
+
+/*-----------------------------------------------------*/
+$sql3="select count(*) as ccount from dbo.Master_Ticket_Tab where Assign_to like '".$resolver[0]."' and Status='AUI'";
+//echo $sql3;
+
+     $result=$conn->query($sql3);
+//echo $msg;
+  while($row3=$result->fetch())
+{
+	if($row3['ccount']!=0)
+	{
+	$sql="update Status_count set Aui=".$row3['ccount']." where Email like '%".$r."%'";
+	//echo $resolver[0].",Classify: $row3['ccount']";
+	//echo $sql;
+	
+	//=$resolver[0].",Classify: $classify,wip:$wip,aui:$auc";
+
+	}
+  else{
+    $sql="update Status_count set Aui=0 where Email like '%".$r."%'";
+  }
+  $conn->query($sql);
+	//$rows[]=$row3;
+
+
+}
+
+
 $sql3="select count(*) as ccount from dbo.Master_Ticket_Tab where Assign_to like '".$resolver[0]."' and Status='Documentation'";
 //echo $sql3;
 
