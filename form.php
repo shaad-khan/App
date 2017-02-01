3ed4789f6b277fb89a9e82397e81cd07094b787a
Shaad Khan <shaad39@gmail.com>
2017-02-01 15:09:19 +0530

    c

--- form.php
+++ form.php
@@ -487,6 +487,8 @@ td
 <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
      <div class="panel panel-default">
   <div class="panel-body">
     
