44d1fa5035430552bf9b66b34a3ff730d3ddb047
Shaad Khan <shaad39@gmail.com>
2017-02-01 15:06:57 +0530

    v

--- form.php
+++ form.php
@@ -487,7 +487,9 @@ td
 <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
-      <div class="panel panel-info">
+     <div class="panel panel-default">
+  <div class="panel-body">
+    
                                   <textarea name="email_chain" cols="50" rows="16" id="pw" value="" placeholder="comments" data-theme="a">
                                     <?php
 //$lk= "/automation/resource/{{ID}}.txt";
@@ -507,7 +509,10 @@ fclose($myfile);
 
                                 </textarea>
 
-                                </div>
+                               
+  </div>
+  <div class="panel-footer"></div>
+</div>
     </div>
   </div>
 </div>
