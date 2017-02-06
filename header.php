ba60a68e4b7806c2695d03883a658a106781cd61
Shaad Khan <shaad39@gmail.com>
2017-02-06 18:31:00 +0530

    z

--- header.php
+++ header.php
@@ -409,14 +409,15 @@ echo $nc;
                 <!--  notification end -->
             </div>
             <div class="top-menu">
-<div class="col-xs-4"><div class="form-group" style="position:relative;left: -80px;">
+
+
+              <ul class="nav pull-right top-menu">
+                  <li> <div class="col-xs-4"><div class="form-group" style="position:relative;left: -80px;">
     
     
     <input type="Text" class="form-control" ng-model="fill" style="width:202px" placeholder="Filter Box"/>
     
-  </div> 
-
-              <ul class="nav pull-right top-menu">
+  </div> </li>
                     <li><a class="logout" href="logout.php">Logout</a></li>
               </ul>
             </div>
