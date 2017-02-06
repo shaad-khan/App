620005c08b28ccb382a3a89ddb03cc7d064f2d14
Shaad Khan <shaad39@gmail.com>
2017-02-06 15:36:29 +0530

    x

---
+++ list.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="continuity_form">
+<head>
+  <title>List Data</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
+   <script src="https://apps.continuserve.com/angular/Contoller_form.js"></script>
+   <script src="angular/Controller_form.js"></script>
+  <?php
+  session_start();
+
+$user_session=$_SESSION["user"];
+if($user_session=='')
+{
+   echo "<script> alert('Session Expired Please Relogin in app');
+     setTimeout(function(){window.close()}, 1000);
+     </script>";
+}
+  $param=$_GET['param'];
+  ?>
+  <style>
+  body{
+    background-color:#001a33;
+  }
+  .clear{
+
+    padding:5px;
+    
+    
+  }
+  .glyphicon-info-sign
+{
+  /*color:#5bc0de;*/
+  color:red;
+}
+.swid
+{
+  width:100px;
+
+}
+td
+{
+  padding:3px;
+}
+.container
+{
+      width: 1119px;
+}
+
+.material-switch > input[type="checkbox"] {
+    display: none;   
+}
+
+.material-switch > label {
+    cursor: pointer;
+    height: 0px;
+    position: relative; 
+    width: 40px;  
+}
+
+.material-switch > label::before {
+    background: rgb(0, 0, 0);
+    box-shadow: inset 0px 0px 10px rgba(0, 0, 0, 0.5);
+    border-radius: 8px;
+    content: '';
+    height: 16px;
+    margin-top: -8px;
+    position:absolute;
+    opacity: 0.3;
+    transition: all 0.4s ease-in-out;
+    width: 40px;
+}
+.material-switch > label::after {
+    background: rgb(255, 255, 255);
+    border-radius: 16px;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
+    content: '';
+    height: 24px;
+    left: -4px;
+    margin-top: -8px;
+    position: absolute;
+    top: -4px;
+    transition: all 0.3s ease-in-out;
+    width: 24px;
+}
+.material-switch > input[type="checkbox"]:checked + label::before {
+    background: inherit;
+    opacity: 0.5;
+}
+.material-switch > input[type="checkbox"]:checked + label::after {
+    background: inherit;
+    left: 20px;
+}
+.tab2>td
+{
+  padding-left:1px;
+
+}
+.form-control {
+
+  height: 28px;
+  
+}
+
+  </style>
+</head>
+<body>
+
+<div class="container">
+<div class="row mt" ng-controller="Ticket">
+   <div class="col-md-12">
+ <div class="content-panel" id="reload">
+
+
+                          <div class="row">
+
+                             <div class="col-xs-12" style="padding-left:40px;padding-right:40px;">
+                               <div class="panel panel-primary">
+                                 <div class="panel-heading" style="background-color:#001a33"><div class="row"><div class="col-xs-8">
+
+                                   Task Report      <a href="https://apps.continuserve.com/main.php#!/" style="color:aqua;font-size:18px;"><span class="glyphicon glyphicon-home"></a></span></div><div class="col-xs-4" align="right" ng-hide="load"><img src="assets/ajax-loader.gif"/>
+                                   </div></div></div>
+       <div class="panel-body" ng-controller="Ticket">
+         <table class="table"><tr>
+     <th>Ticket_ID</th>
+     <th>Client</th>
+     <th>Project</th>
+     <th>Discription</th>
+     <th>Status</th>
+     <th>Creation_DateTime</th>
+     <th>Last_Update_DateTime</th>
+     <th>Creator</th>
+
+
+   </tr>
+
+
+    <tr ng-repeat="res in results" ng-class="$index % 2 > 0 ? 'active':''">
+<td><button class="btn btn-info" ng-click="pop(res.Ticket_ID)"><span class="glyphicon glyphicon-briefcase" aria-hidden="true">
+</span> {{res.Ticket_ID}}</button></td>
+<td>{{res.Client}}</td>
+<td>{{res.Project}}</td>
+<td>{{res.Tdiscription}}</td>
+<td>{{res.Status}}</td>
+<td>{{res.Cdatetime}}</td>
+<td>{{res.Updatetime}}</td>
+<td>{{res.Creator}}</td>
+
+    </tr>
+ </table
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
