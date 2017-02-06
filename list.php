84d828e766135c507ec19db99d1b95de03e76548
Shaad Khan <shaad39@gmail.com>
2017-02-06 18:12:21 +0530

    x

--- list.php
+++ list.php
@@ -122,7 +122,7 @@ td
 
                              <div class="col-xs-12" style="padding-left:40px;padding-right:40px;">
                                <div class="panel panel-primary">
-                                 <div class="panel-heading" style="background-color:#001a33"><div class="row"><div class="col-xs-4">
+                                 <div class="panel-heading" style="background-color:#001a33"><div class="row"><div class="col-xs-2">
 
                                    Task Report </div><div class="col-xs-4"><div class="form-group">
     
