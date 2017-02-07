086e385f4c88b4e3c16e4ef7f060e9f5fc3f43f3
Shaad Khan <shaad39@gmail.com>
2017-02-07 17:46:50 +0530

    x

--- form.php
+++ form.php
@@ -501,9 +501,9 @@ fclose($myfile);
 <div class="modal fade bs-example-modal-lg2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
-       <div class="modal-header"> <h4 class="modal-title" id="myModalLabel">Update History <span class="glyphicon glyphicon-envelope
+       <div class="modal-header"> 
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">Update History <span class="glyphicon glyphicon-comment
 "> </span></h4>
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
      </div> <div class="row clear">
  <div class="col-xs-12">
   
