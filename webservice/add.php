0d3a55b984ea53c90965963c10baecd4654cd9a3
Shaad Khan <shaad39@gmail.com>
2017-02-07 23:56:08 +0530

    z

--- webservice/add.php
+++ webservice/add.php
@@ -187,25 +187,26 @@ if($fstatus=='')
 }
 $update_table_sql="insert into Update_Tab values('$TID','$fstatus','$utime','$uname','$schedule','$client','$project','$ttime','$freviewer','$fresolver','','$tcategory','',$aui_flag,'$comments')";
 //echo $update_table_sql;
-$f=1;
-$conn->query($update_table_sql);
 
+$conn->query($update_table_sql);
+$f2=1;
 $tsql="select TimeTaken from Update_Tab where TicketId='$TID'";
 $result=$conn->query($tsql);
 while($row1=$result->fetch())
 {
   $total=$total+$row1['TimeTaken'];
   }
- $s="Update Master_Ticket_Tab set TotalTime=$total where Ticket_ID='$TID'";
+ $s="Update Master_Ticket_Tab set Total_time=$total where Ticket_ID='$TID'";
 $conn->query($s); 
 }
-if($f==1)
+if($f2==1)
 {
 echo "<script> alert('Updated successfully');
      setTimeout(function(){window.close()}, 1000);
      </script>";
 }
-else{
+else
+{
     echo "<script> alert('Updation failed');
      setTimeout(function(){window.close()}, 1000);
      </script>";
