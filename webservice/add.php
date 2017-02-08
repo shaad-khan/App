223d2ea6b317181b810955e2ff9599bfd0bd8482
Shaad Khan <shaad39@gmail.com>
2017-02-08 18:55:49 +0530

    x

--- webservice/add.php
+++ webservice/add.php
@@ -86,6 +86,7 @@ $Master_sql="Update Master_Ticket_Tab set Assign_To='$user_session',Updatetime='
 $fstatus='AUI';
 $aui_flag=1;
         $fresolver=$user_session;
+        
         $Master_sql="Update Master_Ticket_Tab set Assign_To='unassigned',Status='$fstatus',Resolver='$fresolver',Resolver_Dtime='$fdate',Updatetime='$utime' where Ticket_ID='$TID'";
 
    // $Master_sql="Update Master_Ticket_Tab set Assign_To='unassigned', Status='$fstatus',Updatetime='$utime' where Ticket_ID='$TID'";
