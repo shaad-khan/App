1a971b731c9455e1af2ff75c41ed253e7d4c7fce
Shaad Khan <shaad39@gmail.com>
2017-02-13 15:36:14 +0530

    c

--- ticket.php
+++ ticket.php
@@ -6,7 +6,7 @@ $u=$_SESSION['user'];
 ?>
 
 
- <div class="row mt" ng-controller="Ticket" ng-init="u=<?php echo $u;?>">
+ <div class="row mt"  ng-init="u=<?php echo $u;?>">
    <div class="col-md-12">
  <div class="content-panel" id="reload">
 
@@ -17,7 +17,7 @@ $u=$_SESSION['user'];
                                <div class="panel panel-primary">
                                  <div class="panel-heading" style="background-color:#001a33"><div class="row"><div class="col-xs-8">
 
-                                  <span class="glyphicon glyphicon-menu-hamburger"></span> L3 Support Team 
+                                  <span class="glyphicon glyphicon-menu-hamburger"></span> L3 Support Team  {{u}}
                                     <a href="https://apps.continuserve.com/main.php#!/" style="color:aqua;font-size:18px;">
                                     <span class="glyphicon glyphicon-home"></a></span></div>
                                     <div class="col-xs-4" align="right" ng-hide="load"><img src="assets/ajax-loader.gif"/>
