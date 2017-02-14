79fd74368b3ac51c2764bc9c9c058ba3cb2978d3
Shaad Khan <shaad39@gmail.com>
2017-02-14 18:17:32 +0530

    c

--- webservice/Tab_content.php
+++ webservice/Tab_content.php
@@ -216,18 +216,18 @@ $sql3="select count(*) as ccount from dbo.Master_Ticket_Tab where Assign_to like
 
 }
 
-$sql="select ID,Email,ISNULL(Classify,0) as Classify,ISNULL(Wip,0) as Wip ,ISNULL(Aui,0) as Aui,ISNULL(Review,0) as Review,ISNULL(Doc,0) as Doc,ISNULL(Closure,0) as Closure,ISNULL(Total,0) as Total from status_count order by ID desc";
+$sql="select ID,Email,ISNULL(Classify,0) as Classify,ISNULL(Wip,0) as Wip ,ISNULL(Aui,0) as Aui,ISNULL(Review,0) as Review,ISNULL(Doc,0) as Doc,ISNULL(Closure,0) as Closure,ISNULL(Total,0) as Total from status_count order by ID desc FOR JSON AUTO";
 $result=$conn->query($sql);
 //echo $msg;
   while($row4=$result->fetch())
 {
 
-	$rows[]=$row4;
+	print($row4);
 }
 
-ob_start("ob_gzhandler");
+/*ob_start("ob_gzhandler");
 
 print(json_encode($rows, JSON_NUMERIC_CHECK));
 ob_end_flush();
-exit;
+exit;*/
 ?>
