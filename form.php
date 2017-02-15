f558067147034b16bb12e508a64950bd62bb8142
Shaad Khan <shaad39@gmail.com>
2017-02-16 02:23:31 +0530

    x

--- form.php
+++ form.php
@@ -414,7 +414,6 @@ td
                         </div>
 
   </td>
-  
 <td ng-if="$items[0].Status=='Classify'">
 <div class="form-group" >
     <label for="exampleInputEmail1">Client Ticket number <span class="glyphicon glyphicon-info-sign
