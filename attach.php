9123ecd511ffecd16482333407db5e4d2491f6d8
Shaad Khan <shaad39@gmail.com>
2017-02-23 16:55:31 +0530

    x

---
+++ attach.php
@@ -0,0 +1,15 @@
+<?php
+
+$tid=$_GET['tid'];
+    $zip = new ZipArchive;
+    $download = 'download.zip';
+    $zip->open($download, ZipArchive::CREATE);
+    foreach (glob("..repo/tid*") as $file) { /* Add appropriate path to read content of zip */
+        $zip->addFile($file);
+    }
+    $zip->close();
+    header('Content-Type: application/zip');
+    header("Content-Disposition: attachment; filename = $download");
+    header('Content-Length: ' . filesize($download));
+    header("Location: $download");
+ ?>
