e786fb218aa06edd33fb8aa38b49cb11678f67f0
Shaad Khan <shaad39@gmail.com>
2017-02-23 17:04:31 +0530

    x

--- attach.php
+++ attach.php
@@ -4,7 +4,7 @@ $tid=$_GET['tid'];
     $zip = new ZipArchive;
     $download = 'download.zip';
     $zip->open($download, ZipArchive::CREATE);
-    foreach (glob("repo/tid*") as $file) { /* Add appropriate path to read content of zip */
+    foreach (glob("repo/$tid") as $file) { /* Add appropriate path to read content of zip */
         $zip->addFile($file);
     }
     $zip->close();
