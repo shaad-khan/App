f39e5934671244ad5d13cc1f9210be0281d43a26
Shaad Khan <shaad39@gmail.com>
2017-02-26 20:00:36 +0530

    c

--- webservice/test.php
+++ webservice/test.php
@@ -48,7 +48,8 @@ $sql3="select count(*) as ccount from dbo.Master_Ticket_Tab where Creator like '
 }
 }
     
-$sql="select ID,Email,ISNULL(Classify,0) as Classify,ISNULL(Wip,0) as Wip ,ISNULL(Aui,0) as Aui,ISNULL(Review,0) as Review,ISNULL(Doc,0) as Doc,ISNULL(Closure,0) as Closure,ISNULL(Total,0) as Total,Name from status_count order by ID desc";
+$sql="select ID,Email,ISNULL(Classify,0) as Classify,ISNULL(Wip,0) as Wip ,ISNULL(Aui,0) as Aui,ISNULL(Review,0) as Review,ISNULL(Doc,0) as Doc,ISNULL(Closure,0) as Closure,ISNULL(Total,0) as Total,Name from status_count where Team='$t' order by ID desc";
+echo $sql;
 $result=$conn->query($sql);
 //echo $msg;
   while($row4=$result->fetch())
