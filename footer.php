2113227dab348be2d51c3f61e3ba24e791eabc08
Shaad Khan <shaad39@gmail.com>
2017-02-28 19:15:47 +0530

    x

--- footer.php
+++ footer.php
@@ -176,14 +176,14 @@ $('#datetimepicker_dark').datetimepicker({theme:'dark'})
    <script src="assets/js/jquery.js"></script>-->
 
 
-    <script src="assets/js/bootstrap.min.js"></script>
+  <!--  <script src="assets/js/bootstrap.min.js"></script>
     <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
     <script src="assets/js/jquery.scrollTo.min.js"></script>
     <!--<script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>-->
 
 
     <!--common script for all pages-->
-    <script src="assets/js/common-scripts.js"></script>
+    <!--<script src="assets/js/common-scripts.js"></script>-->
 
     <!--script for this page-->
 
