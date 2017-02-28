fed20f1c78580e57575203f55d4311c85ca67284
Shaad Khan <shaad39@gmail.com>
2017-03-01 04:38:05 +0530

    z

--- task.php
+++ task.php
@@ -22,7 +22,7 @@ $u=$_SESSION['user'];
 
                                     <a href="https://apps.continuserve.com/main.php#!/" style="color:aqua;font-size:18px;">
                                     <span class="glyphicon glyphicon-home"></a></span></div>
-                                   <div class="col-xs-3">
+                                   <div class="col-xs-2">
                                     
                                     
             <div class="form-group">
