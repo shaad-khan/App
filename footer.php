62af5c47ff0878c4a785907988bdae932fb79e9c
Shaad Khan <shaad39@gmail.com>
2017-02-28 14:08:51 +0530

    c

--- footer.php
+++ footer.php
@@ -163,14 +163,14 @@ $('#datetimepicker_dark').datetimepicker({theme:'dark'})
 
 </script>
     <!-- js placed at the end of the document so the pages load faster -->
-    <!-- sumoselect->
-   <!-- <script src="assets/js/jquery.js"></script>-->
+    <!-- sumoselect-->
+   <script src="assets/js/jquery.js"></script>
 
 
     <script src="assets/js/bootstrap.min.js"></script>
     <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
     <script src="assets/js/jquery.scrollTo.min.js"></script>
-    <!--<script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>-->
+    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
 
 
     <!--common script for all pages-->
--- task.php
+++ task.php
@@ -55,7 +55,7 @@ $u=$_SESSION['user'];
 <td>{{res.Status}}</td>
 
 <td>{{res.Updatetime}}</td>
-<td>{{res.Total_time}}</td>
+<td>{{res.Total_time}} min</td>
 
 <!--<td ng-if="res.Blocker_flag==0 && ((res.Status=='WIP')||(res.Status=='AUI'))"><button class="btn btn-info" ng-click="push(res.Ticket_ID)"><span class="glyphicon glyphicon-check"></span></sbutton>
 </td>
