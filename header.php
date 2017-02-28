3e0024ecb285a19aee6ad70e2f487da97c23cde5
Shaad Khan <shaad39@gmail.com>
2017-02-28 18:16:50 +0530

    j

--- header.php
+++ header.php
@@ -52,7 +52,7 @@ $result2=$conn->query($sql2);
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>                       
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
 <script>
 function myFunction1() {
     document.getElementById("demo").innerHTML = "Checklist Task";
