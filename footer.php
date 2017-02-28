f8fb4acfc09a481a44e288066ad42f8662334086
Shaad Khan <shaad39@gmail.com>
2017-02-28 14:59:24 +0530

    c

--- footer.php
+++ footer.php
@@ -34,7 +34,7 @@ function popitup4(url) {
 // -->
 </script>
 <script src="dateresource/jquery.js"></script>
-<script src="dateresource/jquery.datetimepicker.js"></script>
+<!--<script src="dateresource/jquery.datetimepicker.js"></script>
 <script>/*
 window.onerror = function(errorMsg) {
   $('#console').html($('#console').html()+'<br>'+errorMsg)
