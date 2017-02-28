b9f9cc57b2cde93a9181093f6fc72c0d27961247
Shaad Khan <shaad39@gmail.com>
2017-03-01 03:55:04 +0530

    c

--- task.php
+++ task.php
@@ -94,7 +94,7 @@ $u=$_SESSION['user'];
 </td>-->
     </tr>
     <tr>
-    <td colspan="6">Total Time Spend</td><td>{{getTotal()}} min approx {{taskhour}} hours {{getHrs(taskhour)}} </td></tr>
+    <td colspan="6">Total Time Spend</td><td ng-init="t=getTotal()">{{getTotal()}} min approx {{getHrs(t)}} </td></tr>
  </table
 </div>
 </div>
