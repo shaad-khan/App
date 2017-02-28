09b4fd786ce28c78529d0449be516fe6dea1a26c
Shaad Khan <shaad39@gmail.com>
2017-02-28 16:29:27 +0530

    x

--- header.php
+++ header.php
@@ -59,10 +59,9 @@ function myFunction1() {
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css" />
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>   
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>                       
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
 
     <link href="assets/css/bootstrap.css" rel="stylesheet">
@@ -74,8 +73,8 @@ function myFunction1() {
     <!-- Custom styles for this Continuitylate -->
     <link href="assets/css/style2.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
-    <link href="assets/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
-    <script src="assets/js/bootstrap-datetimepicker.min.js"></script>
+   <!-- <link href="assets/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+    <script src="assets/js/bootstrap-datetimepicker.min.js"></script>-->
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
