d3378a67a1f18a68a32cd660f17e8d3d7fdb2232
Shaad Khan <shaad39@gmail.com>
2017-02-28 16:35:39 +0530

    x

--- header.php
+++ header.php
@@ -42,7 +42,7 @@ $result2=$conn->query($sql2);
 
     <title>Continuity App</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 <!-- Optional theme -->
 
