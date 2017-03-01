f836cc7e65a76d38a52e4eadb0f967852e4fcf29
Shaad Khan <shaad39@gmail.com>
2017-03-01 23:52:35 +0530

    c

--- angular/controller.js
+++ angular/controller.js
@@ -267,7 +267,7 @@ var url="list.php?param="+text;
 /*------------------------------------------------------------------*/
 app.controller("Task",function($scope,service,$interval,$http){
 var x;
-service.serv("https://apps.continuserve.com/webservice/task_serv.php").then(function(response){
+/*service.serv("https://apps.continuserve.com/webservice/task_serv.php").then(function(response){
   
      	//$scope.load='false';
       $scope.tasks=response.data;
@@ -276,7 +276,7 @@ service.serv("https://apps.continuserve.com/webservice/task_serv.php").then(func
   
      //	$scope.load='false';
       $scope.atask=response.data;
-    });
+    });*/
 $scope.search=function(s,e)
 {
   $scope.tasks={};
