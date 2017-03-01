0337f6b4e4b21456e7c98f48a439bc6bb75e3862
Shaad Khan <shaad39@gmail.com>
2017-03-01 23:00:58 +0530

    x

--- webservice/tes4.php
+++ webservice/tes4.php
@@ -46,7 +46,7 @@ $sql3="select count(*) as ccount from dbo.Master_Ticket_Tab where Creator like '
 
 }
 $sql3="select count(*) as ccount from dbo.Master_Ticket_Tab where Assign_to like '".$resolver."' and Status='WIP'";
-echo $sql3;
+//echo $sql3;
 
      $result=$conn->query($sql3);
 //echo $msg;
