6e54bc2eef416cf78b504645857b3b16ab2cc99f
Shaad Khan <shaad39@gmail.com>
2017-03-01 16:54:05 +0530

    x

--- task.php
+++ task.php
@@ -79,7 +79,7 @@ $u=$_SESSION['user'];
    </tr>
 
 
-    <tr ng-repeat="res in tasks" ng-class="$index % 2 > 0 ? 'active':''" ng-init="total=0">
+    <tr ng-repeat="res in tasks" ng-class="$index % 2 > 0 ? 'active':''" ng-if="$index>0">
 <!--<td ng-if="res.Blocker_flag==0 && ((res.Status=='WIP')||(res.Status=='AUI'))"><button class="btn btn-info" ng-click="pop(res.Ticket_ID)"><span class="glyphicon glyphicon-briefcase" aria-hidden="true">
 </span> {{res.Ticket_ID}}</button></td>
 <td ng-if="res.Blocker_flag==1 && ((res.Status=='WIP')||(res.Status=='AUI'))&&((u!=res.Blocker_name))"><button class="btn btn-default" ><span class="glyphicon glyphicon-briefcase" aria-hidden="true">
