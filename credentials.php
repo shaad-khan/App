73619e0d5ccfa15c9654cdddcfdc8296aa27fc8f
Shaad Khan <shaad39@gmail.com>
2017-03-27 18:49:22 +0530

    x

--- credentials.php
+++ credentials.php
@@ -38,31 +38,14 @@ $u=$_SESSION['user'];
            <input type="text" class="form-control" value=""  style="color:black" placeholder="Filter keyword" ng-model="ftext"/>
           </div>
                                     
-                                    </div><!--
-                                    <div class="col-xs-2" >
-                                    
-                                    <div class="form-group">
-            
-         <button type="button" class="btn btn-primary" ng-click="search(sdate,edate)"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> </button>
-          
-          </div>
-                                    
                                     </div>
-                                     <div class="col-xs-2">
-                                    
-                                    <div class="form-group">
-            <button type="button" class="btn btn-success" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="glyphicon glyphicon-plus
-" aria-hidden="true"></span>Add Adhoc Task</button>
-        <!-- <button type="button" class="btn btn-primary" ng-click="add(sdate,edate)"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></button>
-          -->
           </div>
                                     
                                     </div>
 
 
 
-                                   <!-- <div class="col-xs-4" align="right" ng-hide="load"><img src="assets/ajax-loader.gif"/>
-                                   </div>--></div></div>
+                                   </div></div>
        <div class="panel-body">
          <table class="table" ><tr>
      <th>Client</th>
