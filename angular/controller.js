faf17606f156ff68d82fff0ef3f5bac8a97ce777
Shaad Khan <shaad39@gmail.com>
2017-03-27 18:18:58 +0530

    c

--- angular/controller.js
+++ angular/controller.js
@@ -198,6 +198,7 @@ service.serv("https://apps.continuserve.com/webservice/credentials_data.php").th
   
      //	$scope.load2='false';
       $scope.creds=response.data;
+      console.log($scope.creds);
     }); 
 
 
--- credentials.php
+++ credentials.php
@@ -18,7 +18,7 @@ $u=$_SESSION['user'];
                                <div class="panel panel-primary">
                                  <div class="panel-heading" style="background-color:#001a33"><div class="row"><div class="col-xs-4">
 
-                                  <span class="glyphicon glyphicon-menu-hamburger"></span> Task List for <?php echo $u;?>
+                                  <span class="glyphicon glyphicon-menu-hamburger"></span> credentials
 
                                     <a href="https://apps.continuserve.com/main.php#!/" style="color:aqua;font-size:18px;">
                                     <span class="glyphicon glyphicon-home"></a></span></div>
