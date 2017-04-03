83f172d6d93419ef394e8ed428ee9813be0bded8
Shaad Khan <shaad39@gmail.com>
2017-04-03 16:13:15 +0530

    c

--- sss/authenticate.php
+++ sss/authenticate.php
@@ -82,7 +82,7 @@ if($f)
 $_SESSION["admin"]=1;
 //$_SESSION["team"]=$team;
 }
-if(($team=="SSS")&&($f==1))
+if(($team=="SSS")||($f==1))
 {
   header('Location:https://apps.continuserve.com/sss/main.php');
 }
