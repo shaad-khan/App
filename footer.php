17bad9791db3ac629b7c4b93ef1f0636ccfc6d07
Shaad Khan <shaad39@gmail.com>
2017-04-07 03:02:48 +0530

    m

--- footer.php
+++ footer.php
@@ -170,65 +170,8 @@ $('#datetimepicker_dark').datetimepicker({theme:'dark'})
     <!-- js placed at the end of the document so the pages load faster -->
     <!-- sumoselect->
    <!-- <script src="assets/js/jquery.js"></script>-->
-<!-------------------------------------------------------------------------- -->
-<script type="text/javascript" src="components/jquery/jquery.min.js"></script>
-	<script type="text/javascript" src="components/underscore/underscore-min.js"></script>
-	<script type="text/javascript" src="components/bootstrap3/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="components/jstimezonedetect/jstz.min.js"></script>
-	<script type="text/javascript" src="js/language/bg-BG.js"></script>
-	<script type="text/javascript" src="js/language/nl-NL.js"></script>
-	<script type="text/javascript" src="js/language/fr-FR.js"></script>
-	<script type="text/javascript" src="js/language/de-DE.js"></script>
-	<script type="text/javascript" src="js/language/el-GR.js"></script>
-	<script type="text/javascript" src="js/language/it-IT.js"></script>
-	<script type="text/javascript" src="js/language/hu-HU.js"></script>
-	<script type="text/javascript" src="js/language/pl-PL.js"></script>
-	<script type="text/javascript" src="js/language/pt-BR.js"></script>
-	<script type="text/javascript" src="js/language/ro-RO.js"></script>
-	<script type="text/javascript" src="js/language/es-CO.js"></script>
-	<script type="text/javascript" src="js/language/es-MX.js"></script>
-	<script type="text/javascript" src="js/language/es-ES.js"></script>
-	<script type="text/javascript" src="js/language/es-CL.js"></script>
-	<script type="text/javascript" src="js/language/es-DO.js"></script>
-	<script type="text/javascript" src="js/language/ru-RU.js"></script>
-	<script type="text/javascript" src="js/language/sk-SR.js"></script>
-	<script type="text/javascript" src="js/language/sv-SE.js"></script>
-	<script type="text/javascript" src="js/language/zh-TW.js"></script>
-	<script type="text/javascript" src="js/language/cs-CZ.js"></script>
-	<script type="text/javascript" src="js/language/ko-KR.js"></script>
-	<script type="text/javascript" src="js/language/id-ID.js"></script>
-	<script type="text/javascript" src="js/language/th-TH.js"></script>
-	<script type="text/javascript" src="js/calendar.js"></script>
-
-  	<script type="text/javascript">
-/*$(document).on("click",".cal-month-day",function(e) {
-      var w = parseInt($(".cal-day-inmonth").css("width"));
-      var x = e.offsetX;
-      var cols = 8; // Really. There are 7 days in a week + 1 header column.
-      var clickedColumn = Math.floor(x / (w/cols));
-      // The date can be found from columns.
-      var dateCol = $(".cal-row-head").find("div").eq(clickedColumn).find("span");
-      var date = dateCol.attr("data-cal-date");
-
-      // Find out the hour clicked
-      var hourtxt = $(this).find(".cal-cell1 b").text()
-      console.log(e.innerText);
-
-console.log(e);
-      console.log(date + " " + hourtxt);
-    });
-*/
 
 
-
-		var disqus_shortname = 'bootstrapcalendar'; // required: replace example with your forum shortname
-		(function() {
-			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-		})();
-	</script>
-<!------------------------------------------------------------------------------- -->
     <script src="assets/js/bootstrap.min.js"></script>
     <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
     <script src="assets/js/jquery.scrollTo.min.js"></script>
