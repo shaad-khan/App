bcc4470251b19cae3d913bc7ab74d88355c29256
Shaad Khan <shaad39@gmail.com>
2017-04-07 03:28:33 +0530

    x

--- cal.php
+++ cal.php
@@ -18,12 +18,12 @@ $admin=$_SESSION["admin"];
                                <div class="panel panel-primary">
                                  <div class="panel-heading" style="background-color:#001a33"><div class="row"><div class="col-xs-8">
 
-                                  <span class="glyphicon glyphicon-menu-hamburger"></span> L3 Support Team 
+                                  <span class="glyphicon glyphicon-menu-hamburger"></span> <?php echo $u;?> Task Calender
 
                                     <a href="https://apps.continuserve.com/main.php#!/" style="color:aqua;font-size:18px;">
                                     <span class="glyphicon glyphicon-home"></a></span></div>
-                                    <div class="col-xs-4" align="right" ng-hide="load"><img src="assets/ajax-loader.gif"/>
-                                   </div></div></div>
+                                   <!-- <div class="col-xs-4" align="right" ng-hide="load"><img src="assets/ajax-loader.gif"/>
+                                   </div>--></div></div>
        <div class="panel-body" style="height: 900px;">
          <iframe class="embed-responsive-item" src="calender.php?user=<?php echo $u;?>"" frameBorder="0" style="width:100%;height:100%;"></iframe>
 </div>
