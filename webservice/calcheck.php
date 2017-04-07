6af09d442c27e4d0ad16e889537ef6aef2132b40
Shaad Khan <shaad39@gmail.com>
2017-04-07 18:28:44 +0530

    x

--- webservice/calcheck.php
+++ webservice/calcheck.php
@@ -12,14 +12,375 @@ $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
     $user=$_GET['u'];
     $date=$_GET['date'];
     $type=$_GET['type'];
-
 if($type==1)
 {
-  $sql="select count(m.Ticket_ID)
-from master_ticket_tab as m ,update_Tab as u 
-where (m.Ticket_ID=u.TicketId and (u.UpdateBy like 'niraj%' or u.Resolver like 'niraj%' )  
-and u.TimeTaken!=0)and CONVERT(date,u.UpdateTime)='$date";
-  //$sql="select count(*) as c from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+  $sql="select count(*) as c from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$tcount=$row4['c'];
+}
+$sql="select sum(TOTAL_TIME) as t from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$ttime=$row4['t'];
+}
+$rows=array('tcount'=>$tcount,'ttime'=>$ttime);
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+}
+//echo $sql;
+/*else
+{
+
+    $sql="select * from ".$type." where Team='L3,SSS' or Team='L3'";
+}
+    //echo $sql;
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$rows[]=$row4;
+}
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+*/
+
+
+?><?php
+$type=$_GET["type"];
+
+$server = "gjtz209gib.database.windows.net";
+$user = "CSL3AppsUser@gjtz209gib";
+$pwd = "C0ntinue2$3rve";
+$db = "CSL2AppsDB";
+
+$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+    $user=$_GET['u'];
+    $date=$_GET['date'];
+    $type=$_GET['type'];
+if($type==1)
+{
+  $sql="select count(*) as c from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$tcount=$row4['c'];
+}
+$sql="select sum(TOTAL_TIME) as t from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$ttime=$row4['t'];
+}
+$rows=array('tcount'=>$tcount,'ttime'=>$ttime);
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+}
+//echo $sql;
+/*else
+{
+
+    $sql="select * from ".$type." where Team='L3,SSS' or Team='L3'";
+}
+    //echo $sql;
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$rows[]=$row4;
+}
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+*/
+
+
+?><?php
+$type=$_GET["type"];
+
+$server = "gjtz209gib.database.windows.net";
+$user = "CSL3AppsUser@gjtz209gib";
+$pwd = "C0ntinue2$3rve";
+$db = "CSL2AppsDB";
+
+$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+    $user=$_GET['u'];
+    $date=$_GET['date'];
+    $type=$_GET['type'];
+if($type==1)
+{
+  $sql="select count(*) as c from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$tcount=$row4['c'];
+}
+$sql="select sum(TOTAL_TIME) as t from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$ttime=$row4['t'];
+}
+$rows=array('tcount'=>$tcount,'ttime'=>$ttime);
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+}
+//echo $sql;
+/*else
+{
+
+    $sql="select * from ".$type." where Team='L3,SSS' or Team='L3'";
+}
+    //echo $sql;
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$rows[]=$row4;
+}
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+*/
+
+
+?><?php
+$type=$_GET["type"];
+
+$server = "gjtz209gib.database.windows.net";
+$user = "CSL3AppsUser@gjtz209gib";
+$pwd = "C0ntinue2$3rve";
+$db = "CSL2AppsDB";
+
+$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+    $user=$_GET['u'];
+    $date=$_GET['date'];
+    $type=$_GET['type'];
+if($type==1)
+{
+  $sql="select count(*) as c from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$tcount=$row4['c'];
+}
+$sql="select sum(TOTAL_TIME) as t from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$ttime=$row4['t'];
+}
+$rows=array('tcount'=>$tcount,'ttime'=>$ttime);
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+}
+//echo $sql;
+/*else
+{
+
+    $sql="select * from ".$type." where Team='L3,SSS' or Team='L3'";
+}
+    //echo $sql;
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$rows[]=$row4;
+}
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+*/
+
+
+?><?php
+$type=$_GET["type"];
+
+$server = "gjtz209gib.database.windows.net";
+$user = "CSL3AppsUser@gjtz209gib";
+$pwd = "C0ntinue2$3rve";
+$db = "CSL2AppsDB";
+
+$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+    $user=$_GET['u'];
+    $date=$_GET['date'];
+    $type=$_GET['type'];
+if($type==1)
+{
+  $sql="select count(*) as c from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$tcount=$row4['c'];
+}
+$sql="select sum(TOTAL_TIME) as t from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$ttime=$row4['t'];
+}
+$rows=array('tcount'=>$tcount,'ttime'=>$ttime);
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+}
+//echo $sql;
+/*else
+{
+
+    $sql="select * from ".$type." where Team='L3,SSS' or Team='L3'";
+}
+    //echo $sql;
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$rows[]=$row4;
+}
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+*/
+
+
+?><?php
+$type=$_GET["type"];
+
+$server = "gjtz209gib.database.windows.net";
+$user = "CSL3AppsUser@gjtz209gib";
+$pwd = "C0ntinue2$3rve";
+$db = "CSL2AppsDB";
+
+$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+    $user=$_GET['u'];
+    $date=$_GET['date'];
+    $type=$_GET['type'];
+if($type==1)
+{
+  $sql="select count(*) as c from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$tcount=$row4['c'];
+}
+$sql="select sum(TOTAL_TIME) as t from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$ttime=$row4['t'];
+}
+$rows=array('tcount'=>$tcount,'ttime'=>$ttime);
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+}
+//echo $sql;
+/*else
+{
+
+    $sql="select * from ".$type." where Team='L3,SSS' or Team='L3'";
+}
+    //echo $sql;
+$result=$conn->query($sql);
+//echo $msg;
+  while($row4=$result->fetch())
+{
+
+	$rows[]=$row4;
+}
+ob_start("ob_gzhandler");
+
+print(json_encode($rows, JSON_NUMERIC_CHECK));
+ob_end_flush();
+exit;
+*/
+
+
+?><?php
+$type=$_GET["type"];
+
+$server = "gjtz209gib.database.windows.net";
+$user = "CSL3AppsUser@gjtz209gib";
+$pwd = "C0ntinue2$3rve";
+$db = "CSL2AppsDB";
+
+$conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+    $user=$_GET['u'];
+    $date=$_GET['date'];
+    $type=$_GET['type'];
+if($type==1)
+{
+  $sql="select count(*) as c from Master_Ticket_Tab where Resolver like '".$user."%' and CONVERT(date,Resolver_Dtime)='$date'";
 $result=$conn->query($sql);
 //echo $msg;
   while($row4=$result->fetch())
