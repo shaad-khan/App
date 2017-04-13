195effd928bca8b7691779f42d32449def39d2ca
Shaad Khan <shaad39@gmail.com>
2017-04-13 17:52:03 +0530

    c

--- task.php
+++ task.php
@@ -226,10 +226,21 @@ $u=$_SESSION['user'];
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+          <h4 class="modal-title">Time Modification Request</h4>
         </div>
         <div class="modal-body">
-          <p><input type="text" name="ticketId"/></p>
+<form>
+        <div class="form-group">
+            <label for="recipient-name" class="control-label">Environment Type</label>
+             
+           
+           <input type="text" class="form-control"  name="ticketId" disabled/>
+          <input type="hidden" ng-model="tid" name="ticketId"/>
+          
+           
+          </div>
+
+         
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
