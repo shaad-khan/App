f2994300fbd713b045b71f7282b9bf0273dd900e
Shaad Khan <shaad39@gmail.com>
2017-05-11 13:00:09 +0530

    x

--- arkive/form.php
+++ arkive/form.php
@@ -224,7 +224,7 @@ td
  
 <div class="form-group">
     <label for="exampleInputEmail1">Assigned To <span class="glyphicon glyphicon-info-sign
-"></span></label> </td> <td><input type="hidden" name="assign" value="{{items[0].Assign_to}}"/>
+"></span></label> </td> <td ng-if="(<?php echo $admin;?>!=1)&&(items[0].Status!='Classify')"><input type="hidden" name="assign" value="{{items[0].Assign_to}}"/>
     <input type="text" class="form-control" id="exampleInputEmail1" placeholder="{{items[0].Assign_to}}" name="aname" disabled>
   </div> </td>
  <td>
