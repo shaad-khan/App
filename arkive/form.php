52571c6b01645ce14af8f7c9bc26fb536858577a
Shaad Khan <shaad39@gmail.com>
2017-05-11 13:21:55 +0530

    x

--- arkive/form.php
+++ arkive/form.php
@@ -449,17 +449,18 @@ td
 <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Next Status" disabled>
   </div>
   </td>
-  <td ng-if="items[0].Status=='Classify'">
+  <!--<td ng-if="items[0].Status=='Classify'">
   <div class="form-group">
     <label for="exampleInputEmail1">Select Team {{items[0].team}} <span class="glyphicon glyphicon-info-sign
 "></span></label></td><td ng-if="items[0].team==null">
    <select class="form-control" name="cteam" >
   <option ng-repeat="t in cteams" value="{{t.Team}}">{{t.Team}}</option>
 </select>
-  </div></td>
+  </div></td>-->
   
-  <td ng-if="items[0].team!=null&& items[0].Status=='Classify'">
-    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="{{items[0].team}}" disabled>
+  <td >
+  <input type="hidden" name="cteam" value="ARKIVE"/>
+    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="ARKIVE" disabled>
   </div></td>
 
   </td>
