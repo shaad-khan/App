d4b6a7925c40becf15792d1dc27bf82f151d1286
Shaad Khan <shaad39@gmail.com>
2017-05-19 16:50:46 +0530

    c

--- arkive/logout.php
+++ arkive/logout.php
@@ -1,8 +1,8 @@
 <?php
 
     session_start();
-    if(isset($_POST['logout'])) {
+   
         session_destroy();
-        header("Location:https://apps.continuserve.com/arkive/");
-        }
+        header('Location:https://apps.continuserve.com/arkive');
+        
 ?>
