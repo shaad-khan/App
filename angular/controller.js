c5dcbb4c96670b165972e04a5e2b1c981ca1e724
Shaad Khan <shaad39@gmail.com>
2017-05-29 01:53:49 +0530

    c

--- angular/controller.js
+++ angular/controller.js
@@ -58,7 +58,7 @@ $http.post('https://apps.continuserve.com/webservice/modify_task2.php', data, co
             .then(function (data, status, headers, config) {
                 $scope.res= data.data;
                 //console.log($scope.res[0].Ticket_ID);
-             alert("Adhoc Ticket updated:" + $scope.res[0].Tid +" and waiting fro admin approval");
+             alert("Adhoc Ticket updated:" + $scope.res[0].Tid +" and waiting for admin approval");
                /* $scope.projecttype='';
                  $scope.tasktype='';
                   $scope.tspent='';
