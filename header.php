2ae79a836c2ae5754d03624de714e8730e3e0c82
Shaad Khan <shaad39@gmail.com>
2017-05-30 17:04:52 +0530

    c

--- header.php
+++ header.php
@@ -35,7 +35,7 @@ $result2=$conn->query($sql2);
   <head>
    <!-- <meta http-equiv="refresh" content="20" />-->
     <meta charset="utf-8">
-    <meta http-equiv="refresh" content="300">
+    <meta http-equiv="refresh" content="1000">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
