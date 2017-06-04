ea96b19435d9012edca40eee17be1d590284ad8f
Shaad Khan <shaad39@gmail.com>
2017-06-04 23:18:35 +0530

    c

--- credentials.php
+++ credentials.php
@@ -57,8 +57,7 @@ padding: 1px;
 <td>{{res.uid}}</td>
 
 <td>{{res.Password}}</td>
-<td><button type="button" class="btn btn-info" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="glyphicon glyphicon-edit
-" aria-hidden="true"></span></button></td>
+<td><button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" data-ticket-id="{{res.Id}}"><span class="glyphicon glyphicon-pencil"></span></button></td>
 
 
     </tr>
@@ -71,7 +70,65 @@ padding: 1px;
 </div>
 </div>
 </div>
-
+<div class="modal fade" id="myModal" role="dialog" ng-controller="modifytask">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Time Modification Request</h4>
+        </div>
+        <div class="modal-body">
+<form>
+        <div class="form-group">
+            <label for="recipient-name" class="control-label">Ticket ID</label>
+             
+           
+           <input type="text" class="form-control"  name="ticketId" disabled/>
+          <input type="hidden" name="ticketId2"  id="selectedDueDate" ng-model="tid" />
+          
+           
+          </div>
+<div class="form-group">
+            <label for="recipient-name" class="control-label">Total Time Spent </label>
+             
+            
+           <input type="number" class="form-control" value="" placeholder="in min" ng-model="tspent2" required/>
+          
+          
+           
+          </div>
+           <div class="form-group">
+            <label for="recipient-name" class="control-label">Select Date:</label>
+             
+            
+           <input type="text" class="form-control" value="" id="some_class_4" name="date" style="color:black" placeholder="Date Time" ng-model="adate2"/>
+          
+          
+           
+          </div>
+           <div class="form-group">
+            <label for="recipient-name" class="control-label">Request By</label>
+             
+            
+           <input type="text" class="form-control" value="" placeholder="<?php echo $u; ?>"  disabled/>
+           <input type="hidden" class="form-control" value="<?php echo $u; ?>"  ng-model="user2" />
+          
+          
+           
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" ng-click="modify(tid,tspent2,adate2,user2)">Modify Task</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
 
 
 
