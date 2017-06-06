bda77752cc4d8f8d2e6e73f02810d0f54d7f14c2
Shaad Khan <shaad39@gmail.com>
2017-06-06 14:15:06 +0530

    c

--- credentials.php
+++ credentials.php
@@ -33,12 +33,7 @@ padding: 1px;
            <input type="text" class="form-control" value=""  style="color:black" placeholder="Filter keyword" ng-model="ftext"/>
          
           </div>
-                                <div class="col-xs-2" style="padding-left: 400px">
-                                    
-                                    <div class="form-group">
-            
-          <button type="button" class="btn btn-success">+ADD</button>
-          </div>    
+                          <button type="button" class="btn btn-success">+ADD</button>           
                                     </div></div></div>
        <div class="panel-body" style="padding: 3px;">
          <table class="table table-hover table-condensed table-striped" ><tr>
