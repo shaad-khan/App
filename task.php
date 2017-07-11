86e50de8d3083c2062de3422394f7e4fd042ffed
Shaad Khan <shaad39@gmail.com>
2017-07-11 15:45:27 +0530

    c

--- task.php
+++ task.php
@@ -208,7 +208,7 @@ header("Location: https://apps.continuserve.com");
           </div>
           <div class="form-group">
             <label for="message-text" class="control-label">Message:</label>
-            <textarea class="form-control" id="message-text" ng-model='amessage' >dsdsdsdsd</textarea>
+            <textarea class="form-control" name="txt" id="message-text" ng-model='amessage' >{{tasktype}}</textarea>
            
           </div>
         </form>
