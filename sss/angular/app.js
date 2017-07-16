566e31208e729149f84efea79ae491bd20ee9bae
Shaad Khan <shaad39@gmail.com>
2017-07-16 22:35:38 +0530

    c

--- sss/angular/app.js
+++ sss/angular/app.js
@@ -24,6 +24,7 @@ app.config(function($routeProvider) {
     templateUrl:"report.php",
      controller  : "Report"
     })
+    
     .when("/task", {
         templateUrl : "task.php",
          controller: 'Task'
