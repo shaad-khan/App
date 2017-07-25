1ee485a448681d65f74509bfcdb5f6aeb1eedc07
Shaad Khan <shaad39@gmail.com>
2017-07-25 15:21:36 +0530

    s

--- sss/webservice/add.php
+++ sss/webservice/add.php
@@ -467,7 +467,8 @@ if($cl_tkt!=null)
 
     /*----------------------------------------------------------------------------------------*/
 echo "<script> alert('Updated successfully');
-window.location = 'https://apps.continuserve.com/sss/form.php?ID='.$TID
+alert('https://apps.continuserve.com/sss/form.php?ID=$TID')
+window.location = 'https://apps.continuserve.com/sss/form.php?ID='".$TID."
 </script>";
     
 header("https://apps.continuserve.com/sss/form.php?ID=".$TID);
