1237b2fef85f3df1f294fa379f0b4b76b7a969c6
Shaad Khan <shaad39@gmail.com>
2017-07-27 14:09:36 +0530

    sss

--- sss/form.php
+++ sss/form.php
@@ -383,6 +383,7 @@ td
     <label for="exampleInputEmail1">Environment Type <span class="glyphicon glyphicon-info-sign
 "></span></label></td><td ng-if="items[0].EnvType==''">
      <select class="form-control" name="env">
+     <option value="General">General</option>
   <option value="Prod">Prod</option>
     <option value="Non-Prod">Non-Prod</option>
 </select>
