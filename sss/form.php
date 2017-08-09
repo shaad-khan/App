beef929818c04e652b99800a689b4a0cbeb88657
Shaad Khan <shaad39@gmail.com>
2017-08-09 18:07:43 +0530

    s

--- sss/form.php
+++ sss/form.php
@@ -220,7 +220,7 @@ td
     <label for="exampleInputEmail1">Creation Date Time <span class="glyphicon glyphicon-info-sign
 "></span></label> </td><!--<input type="hidden" value="{{date | date:'yyyy-MM-dd HH:mm:ss'}}" />-->
      <td ng-show="items[0].Status=='Classify'">
-     <input type="text" class="form-control"  id="datetimepicker_dark"  placeholder="Creation Date Time" name="crtime" required/>
+     <input type="text" class="form-control"  id="datetimepicker_dark"  placeholder="Creation Date Time" name="crtime" />
      
   
    <!--  <input type="text" class="form-control"  id="exampleInputEmail1" placeholder="{{date | date:'yyyy-MM-dd HH:mm:ss'}}" name="utime">
