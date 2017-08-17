638f052cacb2a24e187b8fab535c7db58f6d0b34
Shaad Khan <shaad39@gmail.com>
2017-08-17 17:07:46 +0530

    s

--- ticket.php
+++ ticket.php
@@ -26,8 +26,8 @@ $admin=$_SESSION["admin"];
                                    </div></div></div>
        <div class="panel-body" style="padding:0px">
          <table class="table"><tr>
-     <th >Ticket_ID</th>
-     <th ng-show="type!='Review'">Client</th>
+     <th ng-show="type!='Closure'" >Ticket_ID</th>
+     <th ng-show="type!='Review' || type!='Closure'">Client</th>
      <th>Project</th>
      <th>Discription</th>
      <th>Status</th>
@@ -50,7 +50,7 @@ $admin=$_SESSION["admin"];
  {{res.Ticket_ID}}</a></td>
  <td ng-if="(res.Status=='Classify')"><button class="btn btn-info" ng-click="pop(res.Ticket_ID)"><span class="glyphicon glyphicon-briefcase" aria-hidden="true">
 </span> {{res.Ticket_ID}}</button></td>
-<td ng-show="type!='Review'">{{res.Client}}</td>
+<td ng-show="type!='Review' || type!='Closure'">{{res.Client}}</td>
 <td>{{res.Project}}</td>
 <td>{{res.Tdiscription}}</td>
 <td>{{res.Status}}</td>
