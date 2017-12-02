d786675609f15c27c4955cab2df0f670a6cf32eb
Shaad Khan <shaad39@gmail.com>
2017-12-03 04:06:33 +0530

    sss

--- sss/form.php
+++ sss/form.php
@@ -196,8 +196,9 @@ td
   <option ng-repeat="project in projects" value="{{project.Project}}">{{project.Project}}</option>
 </select>
   </div></td>
-  
+  <input type="hidden" value="{{project.Project}}"" name="project"/>
   <td ng-if="items[0].Project!=''">
+
     <input type="text" class="form-control" id="exampleInputEmail1" placeholder="{{items[0].Project}}" disabled>
   </div></td>
   
