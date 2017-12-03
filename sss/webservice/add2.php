efb2ba1390b577152ec08b2e6a927d5e4a012c6e
Shaad Khan <shaad39@gmail.com>
2017-12-03 22:21:46 +0530

    sss

--- sss/webservice/add2.php
+++ sss/webservice/add2.php
@@ -480,7 +480,9 @@ if($cl_tkt!=null)
 
 
     /*----------------------------------------------------------------------------------------*/
-
+echo "<script> alert('Updated successfully');
+window.location='https://apps.continuserve.com/sss/form.php?ID=$TID';
+</script>";
     
 //header("https://apps.continuserve.com/sss/form.php?ID=".$TID);
 }
