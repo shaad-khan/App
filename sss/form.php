2cc30310952aa062dd6f85763abcebbe712ad4f8
Shaad Khan <shaad39@gmail.com>
2017-12-04 13:14:02 +0530

    ss

--- sss/form.php
+++ sss/form.php
@@ -237,7 +237,7 @@ td
     <label for="exampleInputEmail1">Creation Date Time <span class="glyphicon glyphicon-info-sign
 "></span></label> </td><input type="hidden" name="crtime" value="{{items[0].Cdatetime}}" />
      <td ng-if="items[0].Status=='WIP' && items[0].Cdatetime">
-     <input type="text" class="form-control" id="datetimepicker_dark"  style="color:black" placeholder="{{items[0].Cdatetime}}"  name="crtime" enabled/>
+     <input type="text" class="form-control" id="datetimepicker_dark"  style="color:black"   name="crtime" enabled/>
      
 
    <!--  <input type="text" class="form-control"  id="exampleInputEmail1" placeholder="{{date | date:'yyyy-MM-dd HH:mm:ss'}}" name="utime">
