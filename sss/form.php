9cae0971d469fda1c04ecd482e40f7e3a639f450
Shaad Khan <shaad39@gmail.com>
2017-12-04 13:45:06 +0530

    wq

--- sss/form.php
+++ sss/form.php
@@ -813,7 +813,7 @@ window.onerror = function(errorMsg) {
            $('#some_class_2').datetimepicker();
             $('#some_class_3').datetimepicker();
              $('#some_class_4').datetimepicker();
-             $('.some_class').datetimepicker();
+             //$('.some_class').datetimepicker();
              $('#datetimepicker_dark').datetimepicker();
               });
          </script>
