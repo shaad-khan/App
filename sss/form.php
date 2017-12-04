37c498f1432644d4f08006730d0511867073ea79
Shaad Khan <shaad39@gmail.com>
2017-12-04 13:25:04 +0530

    ww

--- sss/form.php
+++ sss/form.php
@@ -224,7 +224,7 @@ td
     <label for="exampleInputEmail1">Creation Date Time <span class="glyphicon glyphicon-info-sign
 "></span></label> </td><!--<input type="hidden" value="{{date | date:'yyyy-MM-dd HH:mm:ss'}}" />-->
      <td ng-show="items[0].Status=='WIP' && !items[0].Cdatetime">
-     <input type="text" class="form-control"  id="some_class_1"  placeholder="Creation Date Time" name="crtime"/>
+     <input type="text" class="form-control"  id="some_class_2" style="color:black"  placeholder="Creation Date Time" name="crtime"/>
 
      
   
@@ -237,7 +237,7 @@ td
     <label for="exampleInputEmail1">Creation Date Time <span class="glyphicon glyphicon-info-sign
 "></span></label> </td><input type="hidden" name="crtime" value="{{items[0].Cdatetime}}" />
      <td ng-if="items[0].Status=='WIP' && items[0].Cdatetime">
-     <input type="text" class="form-control" id="some_class_1"  style="color:black" placeholder="{{items[0].Cdatetime}}"  name="crtime" enabled/>
+     <input type="text" class="form-control" id="some_class_2"  style="color:black" placeholder="{{items[0].Cdatetime}}"  name="crtime" enabled/>
      
 
    <!--  <input type="text" class="form-control"  id="exampleInputEmail1" placeholder="{{date | date:'yyyy-MM-dd HH:mm:ss'}}" name="utime">
