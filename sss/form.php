7576b8c6a1a58093ee73e25eae4a41199c36f81a
Shaad Khan <shaad39@gmail.com>
2018-01-03 14:32:59 +0530

    ss
    dd

--- sss/form.php
+++ sss/form.php
@@ -224,7 +224,7 @@ td
     <label for="exampleInputEmail1">Creation Date Time <span class="glyphicon glyphicon-info-sign
 "></span></label> </td><!--<input type="hidden" value="{{date | date:'yyyy-MM-dd HH:mm:ss'}}" />-->
      <td ng-show="items[0].Status=='WIP' && !items[0].Cdatetime">
-     <input type="datetime-local" class="form-control hasDatepicker"   style="color:black"  placeholder="Creation Date Time" name="crtime"/>
+     <input type="text"  class="form-control"  id="some_class_2"   style="color:black"  placeholder="Creation Date Time" name="crtime"/>
 
      
   
