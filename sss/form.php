5c48be36c421d206913dd1c427de45d5a15a918e
Shaad Khan <shaad39@gmail.com>
2018-05-20 05:24:58 +0530

    ada

--- sss/form.php
+++ sss/form.php
@@ -562,7 +562,7 @@ td
 <td ng-if="(items[0].Status=='WIP')||(items[0].Status=='AUI')">
 <div class="form-group"  >
     <label for="exampleInputEmail1">If awaiting for User response<span class="glyphicon glyphicon-info-sign
-"></span></label></td><td ng-if="(items[0].Status=='WIP')||(items[0].Status=='AUI')" style="position:relative;left: -119px;;">
+"></span></label></td><td ng-if="(items[0].Status=='WIP')||(items[0].Status=='AUI')" style="position:relative;left: -110px;;">
 
   
   <div class="material-switch pull-right">
