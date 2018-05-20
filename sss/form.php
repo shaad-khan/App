9b6b3f0bf08abf7ce8a5f5a59b7544f8c5e0d3a2
Shaad Khan <shaad39@gmail.com>
2018-05-21 01:00:17 +0530

    sd

--- sss/form.php
+++ sss/form.php
@@ -664,7 +664,7 @@ td
   </div>
     </td>
     <td ng-if="items[0].Status!='Closure' && items[0].Status!='Close'">
-    <input type="number" class="form-control" name="ttime" id="exampleInputEmail1" placeholder="Time In min" required>
+    <input type="number" class="form-control" name="ttime" id="exampleInputEmail1" value="{{nitems[0].Mins}}" placeholder="Time In min" required>
   </div>
 
     </td>
