52a3f3fa56c64987efd936dd146344c8dc9e2421
Shaad Khan <shaad39@gmail.com>
2018-05-25 15:08:38 +0530

    vv

--- sss/form.php
+++ sss/form.php
@@ -161,7 +161,7 @@ td
 "></span></button> <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg2"><span class="glyphicon glyphicon-comment
 "></span></button>&nbsp;<button type="button" class="btn btn-primary"><a href="<?php Echo $link;?>" style="text-decoration:none;color:white"><span class="glyphicon glyphicon-paperclip
 "></span></a></button>  [ Ticket Description : {{items[0].Tdiscription}}]  [Assigned To : {{items[0].Assign_to}}] [Status : {{items[0].Status}}]</div>
-    <div class="panel-body" ng-init="User='<?php Echo $user_session;?>'"><form class="form-inline" action="webservice/add2.php" method="POST" enctype="multipart/form-data">
+    <div class="panel-body" ng-init="User='<?php Echo $user_session;?>'"><form class="form-inline" action="webservice/add2.php" method="GET" enctype="multipart/form-data">
 
 <table>
 <tr align="center"><!--<td>
